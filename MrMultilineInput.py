249fd68d220fcd9abfe29635b88c71830573e411
Stjernelund <59571307+Stjernelund@users.noreply.github.com>
2022-03-10 14:41:29 +0100

    n

--- MrMultilineInput.py
+++ MrMultilineInput.py
@@ -9,22 +9,26 @@ class MRMultilineInput(MRJob):
     
     def mapper(self,_,line):
         line = line.strip()
-        if line[0].isdigit():
+
+        if line and line[0] == '"' and line[1].isdigit():
             line_split = line.split(",")
             self.message_id = line_split[0]
             self.body.append(line[4])
             self.in_body = True
 
-        if self.in_body:
+        elif line.find("<AbstractText") == 0:
             startIndex = line.find(">") + 1
             endIndex = line.find("<",startIndex)
             self.body.append(line[startIndex,endIndex])
 
-        if line.find("</Abstract") and self.in_body:
+        elif line.find("</Abstract") and self.in_body:
             yield self.message_id, ''.join(self.body)
             self.message_id = ''
             self.body = []
             self.in_body = False
+        
+        else:
+            self.in_body = True
             
         
 
