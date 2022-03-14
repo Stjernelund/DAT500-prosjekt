7ef5338dd0334ea901b363f1c794324b42adbc4d
GodNooNoo <emili.haava@gmail.com>
2022-03-14 15:15:43 +0100

    Update MrNgram.py

--- MrNgram.py
+++ MrNgram.py
@@ -1,18 +1,14 @@
 from mrjob.job import MRJob
+import nltk
 
 class MRNgram(MRJob):
 
     def mapper(self, _, line):
-        line = line.strip() # remove leading and trailing whitespace
-        if line.find("From:") == 0:
-            email_domain = line[line.find("@")+1:line.find(">")]
-            if len(email_domain) == 0:
-                email_domain == "empty"
-            yield email_domain, 1
+        yield set(nltk.ngrams(line.split(), 2))
 
     def combiner(self, key, values):
         yield key, sum(values)
-        
+
     def reducer(self, key, values):
         yield key, sum(values)
 
