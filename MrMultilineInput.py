fedbb98d126262fcd4941a4a61a3b12648c41a0f
Stjernelund <59571307+Stjernelund@users.noreply.github.com>
2022-03-14 13:57:57 +0100

    Update MrMultilineInput.py

--- MrMultilineInput.py
+++ MrMultilineInput.py
@@ -1,3 +1,6 @@
+from email import message
+
+from matplotlib.pyplot import title
 from mrjob.job import MRJob
 
 class MRMultilineInput(MRJob):
@@ -22,16 +25,15 @@ class MRMultilineInput(MRJob):
             if message_id != "":
                 self.message_id = message_id
                 title_temp = line[split_indices[3] + 1:split_indices[4]]
-                title_str = ''.join([i for i in title_temp if i.isalpha() or i == " "])
-                self.body.append(title_str)
+                title = ''.join([i for i in title_temp if i.isalpha() or i == " "])
+                self.body.append(title)
                 self.in_body = True
 
         elif line.find("<AbstractText") == 0:
             startIndex = line.find(">") + 1
             endIndex = line.find("<",startIndex)
-            abs_temp = line[line[startIndex:endIndex]]
+            abs_temp = line[startIndex:endIndex]
             abs = ''.join([i for i in abs_temp if i.isalpha() or i == " "])
-            self.body.append(abs)
 
         elif line.find("</Abstract") and self.in_body:
             yield self.message_id, ''.join(self.body)
