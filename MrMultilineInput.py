a1114817e6bd25170f7fc50532e9e671875f167a
GodNooNoo <emili.haava@gmail.com>
2022-03-18 13:39:22 +0100

    Update MrMultilineInput.py

--- MrMultilineInput.py
+++ MrMultilineInput.py
@@ -5,6 +5,7 @@ class MRMultilineInput(MRJob):
     def mapper_init(self):
         self.message_id = ''
         self.in_body = False
+        self.body = []
 
     def mapper(self, _, line):
         line = line.strip()
@@ -23,36 +24,24 @@ class MRMultilineInput(MRJob):
                 self.message_id = message_id
                 title_temp = line[split_indices[3] + 1:split_indices[4]]
                 title = ''.join([i for i in title_temp if i.isalnum() or i == " "]).lower()
+                self.body.append(title)
                 self.in_body = True
-                for word in title:
-                    yield self.message_id, word
 
         elif line.find("<AbstractText") != -1 and self.in_body:
             startIndex = line.find(">") + 1
-            endIndex = line.find("<",startIndex)
+            endIndex = line.find("<", startIndex)
             abs_temp = line[startIndex:endIndex]
             abs = ''.join([i for i in abs_temp if i.isalnum() or i == " "]).lower()
-            for word in abs:
-                yield self.message_id, word
+            self.body.append(abs)
 
         elif line.find("</Abstract") != -1 and self.in_body:
+            yield self.message_id, ''.join(self.body).lower()
             self.message_id = ''
+            self.body = []
             self.in_body = False
-            yield None, None
-        
-        if line.find("<p/>") != -1:
-            self.in_body = False
-            yield None, None
-            
-        else:
-            self.in_body = True
-            yield None, None
 
-    def combiner(self, message_id, words):
-        yield message_id, list(words)
 
-    def reducer(self, message_id, words):
-        yield message_id, ''.join(words)
-
+if __name__ == '__main__':
+    MRMultilineInput.run()
 if __name__ == '__main__':
     MRMultilineInput.run()
