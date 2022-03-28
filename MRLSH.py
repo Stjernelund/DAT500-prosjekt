185cbf58ed399b466a6843a15a45b781c94d26d0
Stjernelund <59571307+Stjernelund@users.noreply.github.com>
2022-03-28 15:32:06 +0200

    Update MRLSH.py

--- MRLSH.py
+++ MRLSH.py
@@ -10,8 +10,7 @@ class MRLSH(MRJob):
     def steps(self):
         return [
             MRStep(mapper_init = self.mapper_init, mapper=self.mapper_pre),
-            # MRStep(mapper = self.mapper_ngram, combiner = self.combiner_ngram, reducer=self.reducer_ngram)
-            MRStep(mapper = self.mapper_ngram)
+            MRStep(mapper = self.mapper_ngram, combiner = self.combiner_ngram, reducer=self.reducer_ngram)
         ]
     def mapper_init(self):
         self.message_id = ''
@@ -56,9 +55,7 @@ class MRLSH(MRJob):
             self.body.append(abs)
 
     def mapper_ngram(self, paper_id, text):
-        splits = text.split('"')
-        #for word in splits[1]:
-        yield paper_id, splits
+        yield paper_id, text
 
     def combiner_ngram(self, paper_id, words):
         ngrams = set(nltk.ngrams(words, 2))
