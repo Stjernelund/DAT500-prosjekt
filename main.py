e75ef87ef3d457aa6db82be05b33f6f94b898910
GodNooNoo <emili.haava@gmail.com>
2022-03-30 18:48:38 +0200

    g

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 #! /usr/bin/python3
 
+from MROneHot import MROneHot
 from mrjob import protocol
 from MRLSH import MRLSH
 
 lsh = MRLSH()
 with lsh.make_runner() as runner:
     runner._input_paths = ['papers2.csv']
+    runner._output_dir = 'outputNgram.txt'
+    runner.run()
+
+
+onehot = MROneHot()
+with onehot.make_runner() as runner:
+    runner._input_paths = ['outputNgram.txt']
     runner._output_dir = 'output'
     runner.run()
-    for _, value in lsh.parse_output(runner.cat_output()):
-        print(value)
-
