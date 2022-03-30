ec673be4baa4d1481f63655d330af0a9a5de0974
GodNooNoo <emili.haava@gmail.com>
2022-03-30 18:33:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from MRLSH import MRLSH
 
 lsh = MRLSH()
 with lsh.make_runner() as runner:
-    runner._input_paths = 'papers2.csv'
-    runner.output_dir = 'outputNgram.txt'
+    runner._input_paths = ['papers2.csv']
+    runner.output_dir = '/home/DAT500-prosjekt/output'
     runner.run()
     for _, value in lsh.parse_output(runner.cat_output()):
         print(value)
