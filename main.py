661369ca179acb3d5c27f950749b6900493d6c71
GodNooNoo <emili.haava@gmail.com>
2022-03-30 18:30:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ from MRLSH import MRLSH
 
 lsh = MRLSH()
 with lsh.make_runner() as runner:
-    runner._input_paths = ['papers2.csv']
-    runner.OUTPUT_PROTOCOL = protocol.JSONValueProtocol
+    runner._input_paths = 'papers2.csv'
+    runner.output_dir = 'outputNgram.txt'
     runner.run()
     for _, value in lsh.parse_output(runner.cat_output()):
-        print(type(value))
+        print(value)
 
