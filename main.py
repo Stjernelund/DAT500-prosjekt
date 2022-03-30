174623a8077dbc402acbbd507b0e4e6cd9725d50
GodNooNoo <emili.haava@gmail.com>
2022-03-30 18:23:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 #! /usr/bin/python3
 
-from json import JSONEncoder
+from mrjob import protocol
 from MRLSH import MRLSH
 
 lsh = MRLSH()
 with lsh.make_runner() as runner:
     runner._input_paths = ['papers2.csv']
+    runner.OUTPUT_PROTOCOL = protocol.JSONValueProtocol
     runner.run()
+    print(lsh.cat_output())
+    print(lsh)
     for _, value in lsh.parse_output(runner.cat_output()):
         print(value)
