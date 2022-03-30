40385b0e8d793fda2b7783054b08190ba765cc15
GodNooNoo <emili.haava@gmail.com>
2022-03-30 18:24:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ with lsh.make_runner() as runner:
     runner._input_paths = ['papers2.csv']
     runner.OUTPUT_PROTOCOL = protocol.JSONValueProtocol
     runner.run()
-    print(lsh.cat_output())
-    print(lsh)
+    print(runner.cat_output())
+    print(runner)
     for _, value in lsh.parse_output(runner.cat_output()):
         print(value)
