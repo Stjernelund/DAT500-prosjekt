4739a96f8752e92f14b0c59bf353124689db6764
GodNooNoo <emili.haava@gmail.com>
2022-03-30 18:51:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ from MRLSH import MRLSH
 lsh = MRLSH()
 with lsh.make_runner() as runner:
     runner._input_paths = ['papers2.csv']
-    runner._output_dir = 'outputNgram.txt'
+    runner._output_dir = 'output'
     runner.run()
 
 
 onehot = MROneHot()
 with onehot.make_runner() as runner:
-    runner._input_paths = ['outputNgram.txt']
+    runner._input_paths = ['output/part-00000']
     runner._output_dir = 'output'
     runner.run()
