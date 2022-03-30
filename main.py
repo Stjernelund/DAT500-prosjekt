9ca08730ad3685bb59b5dc35d6d7f1d187bb6a58
GodNooNoo <emili.haava@gmail.com>
2022-03-30 18:19:58 +0200

    f

--- main.py
+++ main.py
@@ -2,24 +2,10 @@
 
 from json import JSONEncoder
 from MRLSH import MRLSH
-from MROneHot import MROneHot
-import io
-
 
 lsh = MRLSH()
-ngrams = None
 with lsh.make_runner() as runner:
     runner._input_paths = ['papers2.csv']
     runner.run()
     for _, value in lsh.parse_output(runner.cat_output()):
-        print(value)
-        ngrams = value
-
-ngrams = JSONEncoder().encode(ngrams)
-
-onehot = MROneHot()
-with onehot.make_runner() as runner:
-    runner._stdin = ngrams
-    runner.run()
-    for _, value in onehot.parse_output(runner.cat_output()):
         print(value)
