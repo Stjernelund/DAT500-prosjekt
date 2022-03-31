d8a3a63e641a05c3157206e58f2df7bc7d444283
GodNooNoo <emili.haava@gmail.com>
2022-03-31 16:12:00 +0200

    Update MRLSH.py

--- MRLSH.py
+++ MRLSH.py
@@ -22,8 +22,8 @@ class MRLSH(MRJob):
     def reduer_LSH(self, _, signature_matrix):
         lsh = LSH.LSH(1)
         for signature in signature_matrix:
-            yield lsh.add_hash(signature)
-            # lsh.add_hash(signature)
+            yield None, lsh.add_hash(signature)
+            lsh.add_hash(signature)
         yield lsh.check_candidates(), lsh.buckets
 
 
