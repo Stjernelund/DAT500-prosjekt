281a9557e1f9834d846754a6a1c6d66d72bea112
GodNooNoo <emili.haava@gmail.com>
2022-03-31 16:33:30 +0200

    Update LSH.py

--- LSH.py
+++ LSH.py
@@ -24,7 +24,7 @@ class LSH:
     def add_hash(self, signature):
         subvecs = self.make_subvecs(signature).astype(str)
         for i, subvec in enumerate(subvecs):
-            subvec = ",".join(np.array2string(subvec))
+            subvec = ",".join(subvec)
             if subvec not in self.buckets[i].keys():
                 self.buckets[i][subvec] = []
             self.buckets[i][subvec].append(self.counter)
