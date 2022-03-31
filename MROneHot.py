a6edf111fc07eee3f5cda8a98479f3167f1ae5e4
Stjernelund <59571307+Stjernelund@users.noreply.github.com>
2022-03-31 11:07:20 +0200

    s

--- MROneHot.py
+++ MROneHot.py
@@ -3,6 +3,7 @@
 from mrjob.job import MRJob
 from mrjob.step import MRStep
 from scipy.sparse import csr_matrix
+import json
 
 class MROneHot(MRJob):
     def steps(self):
@@ -11,6 +12,7 @@ class MROneHot(MRJob):
         ]
 
     def reducer_onehot(self, _, ngrams):
+        ngrams = json.loads(ngrams)
         vocabulary = dict()
         indices = list()
         sparse_data = list()
--- main.py
+++ main.py
@@ -3,16 +3,22 @@
 from MROneHot import MROneHot
 from mrjob import protocol
 from MRLSH import MRLSH
+from MrPreProcess import MRPreProcess
 
-lsh = MRLSH()
-with lsh.make_runner() as runner:
+PreProcess = MRPreProcess()
+with PreProcess.make_runner() as runner:
     runner._input_paths = ['papers2.csv']
     runner._output_dir = 'output'
     runner.run()
-
 
 onehot = MROneHot()
 with onehot.make_runner() as runner:
     runner._input_paths = ['output/part-*']
     runner._output_dir = 'output2'
     runner.run()
+
+#lsh = MRLSH()
+#with lsh.make_runner() as runner:
+#    runner._input_paths = ['output2/part-*']
+#    runner._output_dir = 'output3'
+#    runner.run()
