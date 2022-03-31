d80f21c2ee768ebeacb2aad246d059a00b0e7272
Stjernelund <59571307+Stjernelund@users.noreply.github.com>
2022-03-31 12:45:28 +0200

    Update MRLSH.py

--- MRLSH.py
+++ MRLSH.py
@@ -11,10 +11,7 @@ class MRLSH(MRJob):
             MRStep(reducer=self.reducer_matrix),
         ]
     def reducer_matrix(self, _, binary_matrix):
-        #yield None, GetSignatureMatrix(binary_matrix)
-        for bin in binary_matrix:
-            yield None, bin
-        #yield None, binary_matrix
+        yield None, GetSignatureMatrix(np.matrix(binary_matrix))
 
 
 if __name__ == '__main__':
