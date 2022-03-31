4ef37b4a51079483e646ecb5ca939872bf97570d
GodNooNoo <emili.haava@gmail.com>
2022-03-31 13:58:55 +0200

    Update MRLSH.py

--- MRLSH.py
+++ MRLSH.py
@@ -13,11 +13,11 @@ class MRLSH(MRJob):
             MRStep(reducer=self.reducer_matrix),
         ]
     def reducer_matrix(self, _, binary_matrix):
-        #yield None, GetSignatureMatrix(list(np.matrix(binary_matrix)))
         remove = ['n', 'u', 'l', '\t']
         for b in binary_matrix:
             b = ''.join(c for c in b if not c in remove)
-            yield None, GetSignatureMatrix(json.loads(b)[0])
+            # yield None, GetSignatureMatrix(json.loads(b))
+            yield None, json.loads(b)
 
 
 if __name__ == '__main__':
