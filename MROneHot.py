822f333ef2c0238994c5ffb332891034f58f3953
Stjernelund <59571307+Stjernelund@users.noreply.github.com>
2022-03-31 12:03:17 +0200

    Update MROneHot.py

--- MROneHot.py
+++ MROneHot.py
@@ -3,7 +3,7 @@
 from mrjob.job import MRJob
 from mrjob.step import MRStep
 from scipy.sparse import csr_matrix
-import numpy as np
+import re
 
 class MROneHot(MRJob):
     def steps(self):
@@ -17,6 +17,8 @@ class MROneHot(MRJob):
         sparse_data = list()
         indptr = [0]
         for matrix in ngrams:
+            flat_1 = re.findall(r"\[(.+?)\]", matrix)
+            res = [sub.split(",") for sub in flat_1]
             #for term in ngram:
                 #term = tuple(list(term))
                 #index = vocabulary.setdefault(term, len(vocabulary))
@@ -25,7 +27,7 @@ class MROneHot(MRJob):
             #indptr.append(len(indices))
         #sparse = csr_matrix((sparse_data, indices, indptr), dtype=int)
         #yield None, sparse.toarray().tolist()
-            yield None, np.matrix(matrix)
+            yield None, res
 
 if __name__ == '__main__':
     MROneHot.run()
