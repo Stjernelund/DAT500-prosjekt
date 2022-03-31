42dc8d11eb9fb805a892ef03b050ffe9cd0aed0a
GodNooNoo <emili.haava@gmail.com>
2022-03-31 20:28:05 +0200

    Update LSH.py

--- LSH.py
+++ LSH.py
@@ -25,7 +25,7 @@ class LSH:
         subvecs = self.make_subvecs(signature)[0]
         for i, subvec in enumerate(subvecs):
             subvec = "".join(str(c) for c in subvec)
-            if subvec not in self.buckets.keys():
+            if subvec not in self.buckets[i].keys():
                 self.buckets[i][subvec] = []
             self.buckets[i][subvec].append(self.counter)
         self.counter += 1
