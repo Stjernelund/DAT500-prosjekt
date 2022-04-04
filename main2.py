b5b5037648cf653c117ab6e2eca9ffa0028b9eb4
GodNooNoo <emili.haava@gmail.com>
2022-04-04 12:12:21 +0200

    Update main2.py

--- main2.py
+++ main2.py
@@ -9,14 +9,14 @@ from DataSketchLSH import MRDataSketchLSH
 preprocesser = MRPreProcess()
 with preprocesser.make_runner() as runner:
     runner._input_paths = ["papers2.csv"]
-    runner._output_dir = "output"
+    runner._output_dir = "outputB"
     runner.run()
 
 
 datasketch = MRDataSketchLSH()
 with datasketch.make_runner() as runner:
     runner._input_paths = ["output/part-*"]
-    runner._output_dir = "output4"
+    runner._output_dir = "outputB2"
     runner.run()
     for key, value in datasketch.parse_output(runner.cat_output()):
         print(key, value)
