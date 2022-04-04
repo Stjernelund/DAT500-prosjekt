aaba30cc8de8631326fc77acaa3929b95e961fb3
Stjernelund <59571307+Stjernelund@users.noreply.github.com>
2022-04-04 12:32:06 +0200

    Update LSH.py

--- LSH.py
+++ LSH.py
@@ -27,7 +27,7 @@ class LSH:
         subvecs = self.make_subvecs(signature).astype(str)
         remove = ["n", "u", "l", "\t"]
         for i, subvec in enumerate(subvecs):
-            subvec = "".join(str(x) for x in subvec)
+            subvec = ",".join(str(x) for x in subvec)
             subvec.strip()
             if subvec not in self.buckets[i].keys():
                 self.buckets[i][subvec] = []
