ad5696669a47163e754b6f2cd3665c38728380e8
GodNooNoo <emili.haava@gmail.com>
2022-04-04 19:16:58 +0200

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -12,6 +12,15 @@ def analyse():
         data = json.load(input)
     print(f"Total number of papers with similarity: {len(data.keys()), len(data)}")
 
+    with open("outputB2/part-00000", "r") as input:
+        counter = 0
+        for line in input:
+            amount = line.count("\\")
+            counter += amount
+        print(f"Total number of papers: {counter}.")
+
+    print(f"Percentage of papers with similarity: {len(data.keys()) / counter * 100}%.")
+
 
 if __name__ == "__main__":
     main()
