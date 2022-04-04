47dc016090284fa2e1856811829787dac70d3064
GodNooNoo <emili.haava@gmail.com>
2022-04-04 12:17:24 +0200

    Update DataSketchLSH.py

--- DataSketchLSH.py
+++ DataSketchLSH.py
@@ -17,7 +17,7 @@ class MRDataSketchLSH(MRJob):
         m = MinHash(num_perm=128)
         for d in set(line):
             m.update(d.encode("utf8"))
-        self.mrjobs.append(tuple(key, m))
+        self.mrjobs.append((key, m))
         yield list(key), None
 
 
