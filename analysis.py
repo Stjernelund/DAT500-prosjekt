e394450cd961aaeeb576ffc113b98dc49fe52138
GodNooNoo <emili.haava@gmail.com>
2022-04-04 18:04:20 +0200

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -1,4 +1,17 @@
 #! /usr/bin/python3
 
+import json
+
+
+def main():
+    analyse()
+
+
+def analyse():
+    with open("similarity.json", "w+") as input:
+        data = json.load(input)
+    print(f"Total number of papers with similarity: {len(data.keys()), len(data)}")
+
+
 if __name__ == "__main__":
     main()
