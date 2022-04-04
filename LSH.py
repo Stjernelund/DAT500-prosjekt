e61a44b4c833c6466397c34bbb08d2aa7548ad76
Stjernelund <59571307+Stjernelund@users.noreply.github.com>
2022-04-04 12:18:25 +0200

    Update LSH.py

--- LSH.py
+++ LSH.py
@@ -28,7 +28,7 @@ class LSH:
         remove = ["n", "u", "l", "\t"]
         for i, subvec in enumerate(subvecs):
             subvec = "".join(str(x) for x in subvec)
-            subvec = re.sub(r"[^\n']", ' ', subvec)
+            subvec = re.sub(r"\n'", '', subvec)
             if subvec not in self.buckets[i].keys():
                 self.buckets[i][subvec] = []
             self.buckets[i][subvec].append(self.counter)
