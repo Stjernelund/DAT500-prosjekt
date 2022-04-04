450be52a6fc17cb60196efb0c6bb826fe2c41dce
Stjernelund <59571307+Stjernelund@users.noreply.github.com>
2022-04-04 11:47:54 +0200

    Update MRLSH.py

--- MRLSH.py
+++ MRLSH.py
@@ -20,10 +20,10 @@ class MRLSH(MRJob):
             yield None, GetSignatureMatrix(json.loads(b))
 
     def reduer_LSH(self, _, signature_matrix):
-        lsh = LSH.LSH(3)
+        lsh = LSH.LSH(2)
         for signature in signature_matrix:
             lsh.add_hash(signature)
-        yield lsh.check_candidates, lsh.buckets
+        yield lsh.check_candidates(), lsh.buckets
 
 
 if __name__ == "__main__":
