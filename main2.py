c783f95dadb4ec52c2fccf8f7d291b0f87e9987b
GodNooNoo <emili.haava@gmail.com>
2022-04-04 13:24:45 +0200

    Update main2.py

--- main2.py
+++ main2.py
@@ -6,18 +6,23 @@ from MRLSH import MRLSH
 from MRPreProcess import MRPreProcess
 from DataSketchLSH import MRDataSketchLSH
 import time
+import shutil
 
 start = time.time()
 
+"""
+shutil.rmtree('outputB')
 preprocesser = MRPreProcess()
 with preprocesser.make_runner() as runner:
     runner._input_paths = ["papers.csv"]
     runner._output_dir = "outputB"
     runner.run()
+"""
 
 preprostime = time.time()
 print(preprostime - start)
 
+shutil.rmtree("outputB2")
 datasketch = MRDataSketchLSH()
 with datasketch.make_runner() as runner:
     runner._input_paths = ["outputB/part-*"]
