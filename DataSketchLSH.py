876009f3b0fa92a9c494c6246a4ffc96371a7f0d
GodNooNoo <emili.haava@gmail.com>
2022-04-04 14:31:38 +0200

    Update DataSketchLSH.py

--- DataSketchLSH.py
+++ DataSketchLSH.py
@@ -18,7 +18,7 @@ class MRDataSketchLSH(MRJob):
         for d in line:
             m.update(d.encode("utf8"))
         self.mrjobs.append((key, m))
-        yield None, str(set(line))
+        yield None, str(type(line))
 
     def reducer(self, _, values):
         yield None, list(values)
