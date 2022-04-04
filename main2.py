7916d8f24b37dd14b6f4b106f82f6f43de30365e
GodNooNoo <emili.haava@gmail.com>
2022-04-04 13:10:23 +0200

    Update main2.py

--- main2.py
+++ main2.py
@@ -5,14 +5,18 @@ from mrjob import protocol
 from MRLSH import MRLSH
 from MRPreProcess import MRPreProcess
 from DataSketchLSH import MRDataSketchLSH
+import time
+
+start = time.time()
 
 preprocesser = MRPreProcess()
 with preprocesser.make_runner() as runner:
-    runner._input_paths = ["papers.csv"]
+    runner._input_paths = ["papers2.csv"]
     runner._output_dir = "outputB"
     runner.run()
 
-print("lager minhash")
+preprostime = time.time()
+print(preprostime - start)
 
 datasketch = MRDataSketchLSH()
 with datasketch.make_runner() as runner:
@@ -20,9 +24,11 @@ with datasketch.make_runner() as runner:
     runner._output_dir = "outputB2"
     runner.run()
 
-print("lager lsh")
+minhashtime = time.time()s
+print(minhashtime - preprostime)
 lsh = datasketch.make_LSH()
 
-item = datasketch(datasketch.get_item(1))
+print(time.time() - minhashtime)
+item = datasketch.get_item(1)
 print(item)
 print(lsh.query(item))
