1b9dc757a7f0328fc2ead7d4a6eed0d3fe15f947
GodNooNoo <emili.haava@gmail.com>
2022-04-04 13:34:17 +0200

    Update DataSketchLSH.py

--- DataSketchLSH.py
+++ DataSketchLSH.py
@@ -25,7 +25,7 @@ class MRDataSketchLSH(MRJob):
         yield None, list(values)
 
     def make_LSH(self):
-        lsh = MinHashLSH(threshold=0.5)
+        lsh = MinHashLSH(threshold=0.99, num_perm=128)
         print(len(self.mrjobs))
         i = 0
         for key, m in self.mrjobs:
