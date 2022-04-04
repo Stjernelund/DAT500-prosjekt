b2a28618d09ef0349267631edb23046411105c77
Stjernelund <59571307+Stjernelund@users.noreply.github.com>
2022-04-04 13:29:29 +0200

    s

--- LSH.py
+++ LSH.py
@@ -24,10 +24,10 @@ class LSH:
         return np.stack(subvecs)
 
     def add_hash(self, signature):
-        subvecs = self.make_subvecs(signature).astype(str)
+        subvecs = self.make_subvecs(signature)
         remove = ["n", "u", "l", "\t"]
         for i, subvec in enumerate(subvecs):
-            subvec = ",".join(str(x) for x in subvec)
+            subvec = numpy.array2string(subvec)
             if subvec not in self.buckets[i].keys():
                 self.buckets[i][subvec] = []
             self.buckets[i][subvec].append(self.counter)
