377b1ff0b9ffb30df4624010ef069a1e2b84cd48
Stjernelund <59571307+Stjernelund@users.noreply.github.com>
2022-04-08 15:23:34 +0200

    Update spark_TF-IDF.py

--- spark_TF-IDF.py
+++ spark_TF-IDF.py
@@ -1,5 +1,5 @@
 from mailbox import linesep
-from pyspark.mllib.feature import HashingTF, IDF
+from pyspark.ml.feature import HashingTF, IDF, Tokenizer
 from pyspark.sql.functions import split
 from pyspark.sql.functions import col
 import pyspark.sql.functions as f
@@ -11,34 +11,18 @@ sc = spark.sparkContext
 # The path can be either a single text file or a directory of text files
 path = "preprocess"
 
-#df1 = spark.read.text(path).map(lambda line: line.split(" "))
-#df1 = df1.select(f.split(df1.value,"\\t")).rdd.flatMap(lambda x: x).toDF(schema=["paper_id","text"])
+df1 = spark.read.text(path).map(lambda line: line.split(" "))
+df1 = df1.select(f.split(df1.value,"\\t")).rdd.flatMap(lambda x: x).toDF(schema=["paper_id","text"])
 
-#df1.show()
-documents = sc.textFile(path).map(lambda line: line.split(" "))
+tokenizer = Tokenizer(inputCol="text", outputCol="words")
+wordsData = tokenizer.transform(df1)
 
+hashingTF = HashingTF(inputCol="words", outputCol="rawFeatures", numFeatures=20)
+featurizedData = hashingTF.transform(wordsData)
+# alternatively, CountVectorizer can also be used to get term frequency vectors
 
-hashingTF = HashingTF()
-tf = hashingTF.transform(documents)
+idf = IDF(inputCol="rawFeatures", outputCol="features")
+idfModel = idf.fit(featurizedData)
+rescaledData = idfModel.transform(featurizedData)
 
-# While applying HashingTF only needs a single pass to the data, applying IDF needs two passes:
-# First to compute the IDF vector and second to scale the term frequencies by IDF.
-tf.cache()
-idf = IDF().fit(tf)
-tfidf = idf.transform(tf)
-
-# spark.mllib's IDF implementation provides an option for ignoring terms
-# which occur in less than a minimum number of documents.
-# In such cases, the IDF for these terms is set to 0.
-# This feature can be used by passing the minDocFreq value to the IDF constructor.
-idfIgnore = IDF(minDocFreq=2).fit(tf)
-tfidfIgnore = idfIgnore.transform(tf)
-print("tfidf:")
-for each in tfidf.collect():
-    print(each)
-
-print("tfidfIgnore:")
-for each in tfidfIgnore.collect():
-    print(each)
-
-sc.stop()
+rescaledData.select("paper_id", "features").show()
