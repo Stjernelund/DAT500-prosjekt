31478660ebcd10e3daf8a46a10bbedcd32f594ea
Stjernelund <59571307+Stjernelund@users.noreply.github.com>
2022-04-08 12:44:53 +0200

    Update readspark.py

--- readspark.py
+++ readspark.py
@@ -8,5 +8,6 @@ sc = spark.sparkContext
 # The path can be either a single text file or a directory of text files
 path = "preprocess"
 
-df1 = spark.read.text(path, sep = "\t") 
+#df1 = spark.read.text(path, sep = "\t") 
+df1 = spark.read.option("delimiter", "\t").text(path)
 df1.show()
