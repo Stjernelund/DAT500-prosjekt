3966b70930c7740ee0c415d91b9ccb6bd29bc19c
Stjernelund <59571307+Stjernelund@users.noreply.github.com>
2022-04-08 14:00:26 +0200

    Update spark_TF-IDF.py

--- spark_TF-IDF.py
+++ spark_TF-IDF.py
@@ -15,7 +15,7 @@ path = "preprocess"
 df1 = spark.read.text(path) 
 df1 = df1.select(f.split(df1.value,"\\t")).rdd.flatMap(lambda x: x).toDF(schema=["paper_id","text"])
 df1['text'] = df1.select('text').apply(nltk.word_tokenize)
-df1('paper_id') = df1.select('text').apply(nltk.word_tokenize)
+#df1('paper_id') = df1.select('paper_id').apply(nltk.word_tokenize)
 
 
 hashingTF = HashingTF()
