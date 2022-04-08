4860c0897d8f9a616592b49e54d6f93cc5899bf4
Stjernelund <59571307+Stjernelund@users.noreply.github.com>
2022-04-08 13:03:55 +0200

    Update readspark.py

--- readspark.py
+++ readspark.py
@@ -1,6 +1,6 @@
 from mailbox import linesep
 from os import sep
-
+import pandas as pd
 
 sc = spark.sparkContext
 
@@ -9,6 +9,7 @@ sc = spark.sparkContext
 path = "preprocess"
 
 df = spark.read.text(path)
-df['paper_id','value'] = df['value'].str.split('\t',expand=True) 
+tmpDF = pd.DataFrame(columns=['paper_id','text'])
+tmpDF[['paper_id','text']] = df['value'].str.split('\t', expand=True)
 #df1 = spark.read.option("de", "\t").text(path)
-df.show()
+tmpDF.show()
