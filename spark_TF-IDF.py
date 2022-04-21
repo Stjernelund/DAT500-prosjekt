a40b294bb5f6eed62b9785192e4fc77fc03a1ded
Stjernelund <59571307+Stjernelund@users.noreply.github.com>
2022-04-21 11:40:05 +0200

    Update spark_TF-IDF.py

--- spark_TF-IDF.py
+++ spark_TF-IDF.py
@@ -65,3 +65,4 @@ to_dense_udf = f.udf(lambda x: to_dense(x), VectorUDT())
 
 # create dense vector
 wordsData = wordsData.withColumn("tfidf_features_dense", to_dense_udf('tfidf_features'))
+wordsData.show()
