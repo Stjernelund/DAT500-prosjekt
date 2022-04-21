65c6f32614dff05fe2d226a613e5c847aa531ff4
Stjernelund <59571307+Stjernelund@users.noreply.github.com>
2022-04-21 11:52:50 +0200

    Update spark_TF-IDF.py

--- spark_TF-IDF.py
+++ spark_TF-IDF.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
     # calculate scores
     idf = IDF(inputCol="vectorizer", outputCol="tfidf_features")
     idf_model = idf.fit(wordsData)
-    wordsData = idf_model.transform(wordsData)
+    wordsData = idf_model.transform(wordsData).head(1000)
 
     # dense the current response variable
     def to_dense(in_vec):
