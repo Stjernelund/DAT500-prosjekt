90fc0d96ddec2f1151872596e5e1881b44a507a6
Stjernelund <59571307+Stjernelund@users.noreply.github.com>
2022-04-21 09:50:00 +0200

    Update spark_TF-IDF.py

--- spark_TF-IDF.py
+++ spark_TF-IDF.py
@@ -3,31 +3,32 @@ import pyspark.sql.functions as f
 from pyspark.sql import SparkSession
 
 # Load documents (one per line).
-spark = SparkSession\
-    .builder\
-    .appName("TfIdfExample")\
-    .getOrCreate()
+if __name__ == "__main__":
+    spark = SparkSession\
+        .builder\
+        .appName("TfIdfExample")\
+        .getOrCreate()
 
-# A text dataset is pointed to by path.
-# The path can be either a single text file or a directory of text files
-path = "preprocess"
-df1 = spark.read.text(path)
-df1 = df1.withColumn("paper_id", f.split(f.col("value"), "\\t").getItem(0)).withColumn("text", f.split(f.col("value"), "\\t").getItem(1))
-df1 = df1.select(f.split(df1.value,"\\t")).rdd.flatMap(lambda x: x).toDF(schema=["paper_id","text"])
+    # A text dataset is pointed to by path.
+    # The path can be either a single text file or a directory of text files
+    path = "preprocess"
+    df1 = spark.read.text(path)
+    df1 = df1.withColumn("paper_id", f.split(f.col("value"), "\\t").getItem(0)).withColumn("text", f.split(f.col("value"), "\\t").getItem(1))
+    df1 = df1.select(f.split(df1.value,"\\t")).rdd.flatMap(lambda x: x).toDF(schema=["paper_id","text"])
 
-tokenizer = Tokenizer(inputCol="text", outputCol="words")
-wordsData = tokenizer.transform(df1)
+    tokenizer = Tokenizer(inputCol="text", outputCol="words")
+    wordsData = tokenizer.transform(df1)
 
-hashingTF = HashingTF(inputCol="words", outputCol="rawFeatures", numFeatures=20)
-featurizedData = hashingTF.transform(wordsData)
+    hashingTF = HashingTF(inputCol="words", outputCol="rawFeatures", numFeatures=20)
+    featurizedData = hashingTF.transform(wordsData)
 
-idf = IDF(inputCol="rawFeatures", outputCol="features")
-idfModel = idf.fit(featurizedData)
-rescaledData = idfModel.transform(featurizedData)
+    idf = IDF(inputCol="rawFeatures", outputCol="features")
+    idfModel = idf.fit(featurizedData)
+    rescaledData = idfModel.transform(featurizedData)
 
-rescaledData.select("paper_id", "features").show() 
+    rescaledData.select("paper_id", "features").show() 
 
-spark.stop()
+    spark.stop()
 #hashingTF = HashingTF(inputCol="text",outputCol="words")
 #tf = hashingTF.transform(df1)
 
