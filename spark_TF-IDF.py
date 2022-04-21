77bed327c86e899e58d43dfe770d21cb63cb44cf
Stjernelund <59571307+Stjernelund@users.noreply.github.com>
2022-04-21 12:44:39 +0200

    Update spark_TF-IDF.py

--- spark_TF-IDF.py
+++ spark_TF-IDF.py
@@ -11,31 +11,45 @@ if __name__ == "__main__":
         .config("spark.memory.offHeap.size","10g") \
         .getOrCreate()
 
-    path = "preprocess"
-    df1 = spark.read.text(path)
-    df1 = df1.withColumn("paper_id", f.split(f.col("value"), "\\t").getItem(0)).withColumn("text", f.split(f.col("value"), "\\t").getItem(1))
-    df1 = df1.select(f.split(df1.value,"\\t")).rdd.flatMap(lambda x: x).toDF(schema=["paper_id","text"])
+    try:
+        path = "preprocess"
+        df1 = spark.read.text(path)
+        df1 = df1.withColumn("paper_id", f.split(f.col("value"), "\\t").getItem(0)).withColumn("text", f.split(f.col("value"), "\\t").getItem(1))
+        df1 = df1.select(f.split(df1.value,"\\t")).rdd.flatMap(lambda x: x).toDF(schema=["paper_id","text"])
+    except EOFError as x:
+        print("feil på lesing")
+    
+    try:    
+        tokenizer = Tokenizer().setInputCol("text").setOutputCol("words")
+        wordsData = tokenizer.transform(df1)
 
-    tokenizer = Tokenizer().setInputCol("text").setOutputCol("words")
-    wordsData = tokenizer.transform(df1)
+        # vectorize
+        vectorizer = CountVectorizer(inputCol='words', outputCol='vectorizer').fit(wordsData)
+        wordsData = vectorizer.transform(wordsData)
 
-    # vectorize
-    vectorizer = CountVectorizer(inputCol='words', outputCol='vectorizer').fit(wordsData)
-    wordsData = vectorizer.transform(wordsData)
+        # calculate scores
+        idf = IDF(inputCol="vectorizer", outputCol="tfidf_features")
+        idf_model = idf.fit(wordsData)
+        wordsData = idf_model.transform(wordsData)
+        wordsData = wordsData.limit(20)
+    except EOFError as x:
+        print("feil på andre")
 
-    # calculate scores
-    idf = IDF(inputCol="vectorizer", outputCol="tfidf_features")
-    idf_model = idf.fit(wordsData)
-    wordsData = idf_model.transform(wordsData)
-    wordsData = wordsData.limit(20)
+        # dense the current response variable
+        def to_dense(in_vec):
+            return DenseVector(in_vec.toArray())
 
-    # dense the current response variable
-    def to_dense(in_vec):
-        return DenseVector(in_vec.toArray())
-    to_dense_udf = f.udf(lambda x: to_dense(x), VectorUDT())
+    try:    
+        to_dense_udf = f.udf(lambda x: to_dense(x), VectorUDT())
+    except EOFError as x:
+        print("feil på to_dense")
 
-    # create dense vector
-    wordsData = wordsData.withColumn("tfidf_features_dense", to_dense_udf('tfidf_features'))
+        # create dense vector
+    try:
+        wordsData = wordsData.withColumn("tfidf_features_dense", to_dense_udf('tfidf_features'))
+    except EOFError as x:
+        print("feil på siste")
+        
     print((wordsData.count(), len(wordsData.columns)))
     spark.stop()
 
