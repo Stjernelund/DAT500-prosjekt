8a049246552ce40b4657e6c7b60825416ca3230b
GodNooNoo <emili.haava@gmail.com>
2022-04-28 15:10:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ def main():
         no_numerals = MRNoNumerals()
         with no_numerals.make_runner() as runner:
             if run_hadoop:
-                runner._input_paths = ["hdfs:///preprocess/output2/"]
+                runner._input_paths = ["hdfs:///preprocess/output2/part-00000"]
                 runner._output_dir = "hdfs:///preprocess/output3"
             else:
                 runner._input_paths = ["preprocess/part-*"]
