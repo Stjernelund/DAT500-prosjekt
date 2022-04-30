b4fc3626f1b2629d03ef5026568655412029c3cf
Stjernelund <59571307+Stjernelund@users.noreply.github.com>
2022-04-30 11:22:47 +0200

    s

--- spark_TF-IDF.py
+++ spark_TF-IDF.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
     sc = spark.sparkContext
     os.environ["PYARROW_IGNORE_TIMEZONE"] = "1"
     try:
-        path ="hdfs://namenode:9000/preprocess/output2/part-*"
+        path ="hdfs://namenode:9000/preprocess/part-*"
         df1 = spark.read.text(path)
         df1 = df1.withColumn("paper_id", f.split(f.col("value"), "\\t").getItem(0)).withColumn("text", f.split(f.col("value"), "\\t").getItem(1))
         df1 = df1.select(f.split(df1.value,"\\t")).rdd.flatMap(lambda x: x).toDF(schema=["paper_id","text"])
@@ -41,7 +41,7 @@ if __name__ == "__main__":
     #Spark.ml.feature implementation of IDF
     for i in range(10):
         start = time.time()
-        vectorizer = CountVectorizer(inputCol='words', outputCol='vectorizer').fit(wordsData)
+        vectorizer = CountVectorizer(inputCol='words', outputCol='vectorizer',vocabSize=1000).fit(wordsData)
         wordsData = vectorizer.transform(wordsData)
         idf = IDF(inputCol="vectorizer", outputCol="tfidf_features")
         idf_model = idf.fit(wordsData)
@@ -52,7 +52,7 @@ if __name__ == "__main__":
     for i in range(10):
         start_s = time.time()
         tfidfVectorizer = TfidfVectorizer(norm=None,analyzer='word',
-            tokenizer=dummy_fun,preprocessor=dummy_fun,token_pattern=None,stop_words=my_stop_words,max_features=500)
+            tokenizer=dummy_fun,preprocessor=dummy_fun,token_pattern=None,stop_words=my_stop_words,max_features=1000)
         tf=tfidfVectorizer.fit_transform(corpus)
         tf_df=pd.DataFrame(tf.toarray(), columns = tfidfVectorizer.get_feature_names_out(),index = paper_ids)
         sklearn_time = time.time() - start_s
