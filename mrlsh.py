b32879dbc4408472082e05ba32b173a22d19ed7b
GodNooNoo <emili.haava@gmail.com>
2022-04-30 21:16:56 +0200

    Update mrlsh.py

--- mrlsh.py
+++ mrlsh.py
@@ -25,7 +25,8 @@ class DataSketchLSH(MRJob):
         m = MinHash(num_perm=self.num_prem)
         line = ast.literal_eval(line)
         for d in line:
-            m.update(str(d).encode("utf8"))
+            pass
+            # m.update(str(d).encode("utf8"))
         # lean_m = LeanMinHash(seed=m.seed, hashvalues=m.hashvalues)  # Saves memoryspace
         self.mrjobs.append(1)
         yield None, str(self.mrjobs[0])
