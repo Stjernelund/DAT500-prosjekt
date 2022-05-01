26f21c1f37e574e7cefcc1a31969fc96d93b0cf5
GodNooNoo <emili.haava@gmail.com>
2022-05-01 12:08:30 +0200

    Update mrlsh.py

--- mrlsh.py
+++ mrlsh.py
@@ -15,17 +15,42 @@ class DataSketchLSH(MRJob):
         self.threshold = threshold
 
     def steps(self):
-        return [MRStep(mapper=self.mapper)]
+        return [
+            MRStep(
+                mapper=self.mapper_minhash,
+                mapper=self.mapper_lsh,
+                mapper=self.mapper_similar,
+            )
+        ]
 
-    def mapper(self, _, line):
+    def mapper_init(self):
+        self.lsh = MinHashLSH(threshold=self.threshold, num_perm=self.num_prem)
+
+    def mapper_minhash(self, _, line):
         """MinHash each paper"""
-        key, line = line.split("\t")
-        key = key.strip('\\"')
+        pid, line = line.split("\t")
+        pid = pid.strip('\\"')
         m = MinHash(num_perm=self.num_prem)
         line = ast.literal_eval(line)
         for d in line:
             m.update(str(d).encode("utf8"))
-        yield key, m.digest().tolist()
+        yield pid, m.digest().tolist()
+
+    def mapper_lsh(self, _, line):
+        pid, line = line.split("\t")
+        pid = pid.strip('\\"')
+        hashval = ast.literal_eval(line)
+        m = LeanMinHash(hashvalues=hashval)
+        self.lsh.insert(pid, m)
+        yield pid, hashval
+
+    def mapper_similar(self, _, line):
+        pid, line = line.split("\t")
+        pid = pid.strip('\\"')
+        hashval = ast.literal_eval(line)
+        similars = self.lsh.query(hashval)
+        similars.remove(pid)
+        yield pid, similars
 
 
 '''
@@ -49,24 +74,24 @@ class DataSketchLSH(MRJob):
     def make_LSH(self):
         """Create LSH index from the MinHashes"""
         lsh = MinHashLSH(threshold=self.threshold, num_perm=self.num_prem)
-        for key, m in self.mrjobs:
-            lsh.insert(key, m)
+        for pid, m in self.mrjobs:
+            lsh.insert(pid, m)
         return lsh.get_counts()
 
     def find_similar(self, lsh, hadoop_string):
         """Query each paper against the others looking for similarities"""
         similar = {}
-        for key, job in self.mrjobs:
+        for pid, job in self.mrjobs:
             found = lsh.query(job)
-            found.remove(key)
+            found.remove(pid)
             if found:
-                similar[key] = found
+                similar[pid] = found
         with open(
             f"{hadoop_string}/output_t{int(self.threshold * 100)}/similar.txt",
             "w+",
         ) as output:
-            for key, line in similar.items():
-                output.write(f"{key}\t{line}\n")
+            for pid, line in similar.items():
+                output.write(f"{pid}\t{line}\n")
 '''
 
 if __name__ == "__main__":
