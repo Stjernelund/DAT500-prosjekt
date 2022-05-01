2e46096b1386a708b4754641197e70f7dfd28016
GodNooNoo <emili.haava@gmail.com>
2022-05-01 13:11:05 +0200

    Update mrlsh.py

--- mrlsh.py
+++ mrlsh.py
@@ -32,10 +32,9 @@ class DataSketchLSH(MRJob):
         try:
             pid, line = line.split("\t")
             pid = pid.strip('\\"')
-
-            """m = MinHash(num_perm=self.num_prem)
+            m = MinHash(num_perm=self.num_prem)
             line = ast.literal_eval(line)
-            for d in line:
+            """for d in line:
                 m.update(str(d).encode("utf8"))
             self.lsh.insert(pid, m)
             self.dict[pid] = m"""
