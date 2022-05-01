111dd73973c05cacbcd372d3590307597093df93
GodNooNoo <emili.haava@gmail.com>
2022-05-01 15:38:54 +0200

    Update mrlsh.py

--- mrlsh.py
+++ mrlsh.py
@@ -9,7 +9,7 @@ from datasketch import MinHash, MinHashLSH, LeanMinHash
 
 class DataSketchLSH(MRJob):
     num_prem = 128
-    threshold = 0.9
+    threshold = 0.8
 
     def steps(self):
         return [
