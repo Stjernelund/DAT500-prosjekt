d52c68b30b966d26538022813f466bf204bb51ff
GodNooNoo <emili.haava@gmail.com>
2022-05-01 13:21:39 +0200

    Update mrlsh.py

--- mrlsh.py
+++ mrlsh.py
@@ -37,8 +37,8 @@ class DataSketchLSH(MRJob):
             for d in line:
                 m.update(str(d).encode("utf8"))
             self.lsh.insert(pid, m)
-            """self.dict[pid] = m"""
-            yield None, None
+            self.dict[pid] = m
+            yield pid, str(m)
         except Exception as e:
             yield None, e
 
